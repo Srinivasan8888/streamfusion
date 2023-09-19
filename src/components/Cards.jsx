7c543206841a71525f4d6f6cc625d5ea8e491cfe
Srinivasan8888 <srinivasan.2021@vitstudent.ac.in>
2023-09-19 17:53:48 +0530

    dialy update

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -5,65 +5,56 @@ const Cards = () => {
     <div className="flex-grow ml-[100px] mr-[30px]">
       <div className=" p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
         <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">
-          Work fast from anywhere
+          Welcome to StreamFusion!
         </h5>
         <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
-          Stay up to date and move work forward with Flowbite on iOS & Android.
-          Download the app today.
+          Manage your stream and webinar at your fingertips.
         </p>
         <div className="items-center justify-center space-y-4 sm:flex sm:space-y-0 sm:space-x-4">
+
+        <a
+            href="#"
+            className="w-full sm:w-auto bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+            style={{ textDecoration: 'none' }}
+          >
+            
+            <svg className="mr-3 w-12 h-12" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24"  fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M17 10.5V7c0-.55-.45-1-1-1H4c-.55 0-1 .45-1 1v10c0 .55.45 1 1 1h12c.55 0 1-.45 1-1v-3.5l4 4v-11l-4 4zM14 13h-3v3H9v-3H6v-2h3V8h2v3h3v2z"/></svg>
+            
+            <div className="text-left">
+              <div className="mb-1 text-xs">Live Stream</div>
+              <div className="-mt-1 font-sans text-sm font-semibold">
+                Go live from studio
+              </div>
+            </div>
+          </a>
+
           <a
             href="#"
             className="w-full sm:w-auto bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+            style={{ textDecoration: 'none' }}
           >
-            <svg
-              className="mr-3 w-7 h-7"
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fab"
-              data-icon="apple"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 384 512"
-            >
-              <path
-                fill="currentColor"
-                d="M318.7 268.7c-.2-36.7 16.4-64.4 50-84.8-18.8-26.9-47.2-41.7-84.7-44.6-35.5-2.8-74.3 20.7-88.5 20.7-15 0-49.4-19.7-76.4-19.7C63.3 141.2 4 184.8 4 273.5q0 39.3 14.4 81.2c12.8 36.7 59 126.7 107.2 125.2 25.2-.6 43-17.9 75.8-17.9 31.8 0 48.3 17.9 76.4 17.9 48.6-.7 90.4-82.5 102.6-119.3-65.2-30.7-61.7-90-61.7-91.9zm-56.6-164.2c27.3-32.4 24.8-61.9 24-72.5-24.1 1.4-52 16.4-67.9 34.9-17.5 19.8-27.8 44.3-25.6 71.9 26.1 2 49.9-11.4 69.5-34.3z"
-              ></path>
-            </svg>
+           <svg className="mr-3 w-12 h-12" xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" fill="#FFFFFF"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 7c-2.76 0-5 2.24-5 5s2.24 5 5 5 5-2.24 5-5-2.24-5-5-5zm0-5C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"/></svg>
             <div className="text-left">
-              <div className="mb-1 text-xs">Download on the</div>
-              <div className="-mt-1 font-sans text-sm font-semibold">
-                Mac App Store
+              <div className="mb-1 text-xs">Recording</div>
+              <div className="mt-1 font-sans text-sm font-semibold">
+                Record from the studio
               </div>
             </div>
           </a>
           <a
             href="#"
             className="w-full sm:w-auto bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700"
+            style={{ textDecoration: 'none' }}
           >
-            <svg
-              className="mr-3 w-7 h-7"
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fab"
-              data-icon="google-play"
-              role="img"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 512 512"
-            >
-              <path
-                fill="currentColor"
-                d="M325.3 234.3L104.6 13l280.8 161.2-60.1 60.1zM47 0C34 6.8 25.3 19.2 25.3 35.3v441.3c0 16.1 8.7 28.5 21.7 35.3l256.6-256L47 0zm425.2 225.6l-58.9-34.1-65.7 64.5 65.7 64.5 60.1-34.1c18-14.3 18-46.5-1.2-60.8zM104.6 499l280.8-161.2-60.1-60.1L104.6 499z"
-              ></path>
-            </svg>
+            <svg  className="mr-3 w-12 h-12" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" viewBox="0 0 24 24" fill="#FFFFFF"><g><path d="M21,3H3C1.9,3,1,3.9,1,5v3h2V5h18v14h-7v2h7c1.1,0,2-0.9,2-2V5C23,3.9,22.1,3,21,3z M1,18v3h3C4,19.34,2.66,18,1,18z M1,14 v2c2.76,0,5,2.24,5,5h2C8,17.13,4.87,14,1,14z M1,10v2c4.97,0,9,4.03,9,9h2C12,14.92,7.07,10,1,10z M11,11.09v2L14.5,15l3.5-1.91 v-2L14.5,13L11,11.09z M14.5,6L9,9l5.5,3L20,9L14.5,6z"/><path d="M0,0h24v24H0V0z" fill="none"/></g></svg>
             <div className="text-left">
-              <div className="mb-1 text-xs">Get in on</div>
-              <div className="-mt-1 font-sans text-sm font-semibold">
-                Google Play
+              <div className="mb-1 text-xs">On-Air webinar</div>
+              <div className="mt-1 font-sans text-sm font-semibold">
+                Host webinar on streamfusion
               </div>
             </div>
           </a>
+          
         </div>
       </div>
     </div>
---
+++ src/components/Tabs.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Tabs = () => {
+  return (
+    <div className='container'>
+        
+        <div className='bloc-tabs'>
+            <div className='tabs active-tabs'>Upcoming</div>
+            <div className='tabs'>Pasts</div>
+        </div>
+        
+        <div className='content-tabs'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Tabs
